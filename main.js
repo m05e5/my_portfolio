88a6205a3a608b681cc360f102ce05ac2bb1bc4b
m05e5 <calebmoses1111@gmail.com>
2022-01-17 12:43:35 +0100

    add contact me form

--- main.js
+++ main.js
@@ -20,4 +20,15 @@ function myFunction() {
   // else {
   //   skills.classList.remove("sticky");
   // }
+  const submitBtn = document.getElementById('submit');
+  const message = document.getElementById('form-state');
+  message.style.color = 'red';
+  submitBtn.onclick = function (event) {
+    const email = document.getElementById('email').value;
+    const lowerCaseEmail = email.toLowerCase();
+    if (email !== lowerCaseEmail) {
+      message.textContent = 'Incorrect Email Format';
+      event.preventDefault();
+    }
+  };
 }
